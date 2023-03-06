df02bd590d0e2cfb3e40a00ca010c6e737913c01
Abraham Segura <abrahamdsm299@gmail.com>
2023-03-06 19:50:04 -0400

    formulario de login

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,37 @@
 import './App.css'
+import { useState } from "react";
+import isValideNAme from './services/isvalideName';
+import isValidePassward from './services/isValidePassword';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [user, setUser] = useState({})
+
+  const handelSudmit = evt => {
+    evt.preventDefault()
+    const data = Object.fromEntries(new FormData(evt.target))
+    const validate = {
+      "user-name": isValideNAme(data["user-name"]),
+      "user-password": isValidePassward(data["user-password"])
+    }
+    setUser(validate)
+    evt.target.reset()
+  }
 
   return (
     <div className="App">
-
+      <section>
+        <h2>Ingresar</h2>
+        <article>
+          <form method='post' onSubmit={handelSudmit}>
+            <input type="text" name='user-name' id='userName' placeholder='ingresa tu usuario' />
+            <input type="password" name="user-password" id="userPassword" placeholder='ingresa tu contraseña' />
+            <button>Ingresar</button>
+          </form>
+        </article>
+        {
+          user["user-password"] === null ? <span>Usuario o Contraseña invalida</span> : null
+        }
+      </section>
     </div>
   )
 }
