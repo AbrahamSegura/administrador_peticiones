432feee53740ab12d34e48bf3f04476717595df5
Abraham Segura <abrahamdsm299@gmail.com>
2023-04-04 13:52:40 -0400

    use custom hook

--- src/components/Dashboard/DashBoard.jsx
+++ src/components/Dashboard/DashBoard.jsx
@@ -1,10 +1,10 @@
 import './DashBoard.css'
 import ListElements from '../ListElements/ListElements'
 import Container from '../Container/Container'
-import { useLocation } from 'wouter'
 import addClassNames from '../../services/addClassNames'
+import useRedirection from '../../hooks/useRedirection'
 export default function DashBoard () {
-  const [location] = useLocation()
+  const { location } = useRedirection()
   addClassNames(location)
   return (
     <>
